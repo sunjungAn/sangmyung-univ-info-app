68361d7ccb978f03c93df842809d09c4576bbc5b
newoldname <thjbjs@outlook.com>
2021-10-04 15:25:00 +0900

    "test"

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //현빈이 굳굳
+        //현빈이 굳굳v ddd
     }
 }
