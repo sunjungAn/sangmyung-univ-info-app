eced82d74ea1c0dfd8da6d5503e2149aa48ec982
newoldname <thjbjs@outlook.com>
2021-10-04 15:22:11 +0900

    initial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sangmyung-Univ.-Info-App
+2021 Sangmyung Univ. information application (2021-2 상생플러스 부문 참가)
---
+++ Sangmyung-Univ.-Info-App-main/app/src/main/java/com/example/myapplication/Example1.java
@@ -0,0 +1,44 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Example1 extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            findViewById(R.id.login_button2).setOnClickListener(onClickListener);
+            findViewById(R.id.resister_button).setOnClickListener(onClickListener);
+            findViewById(R.id.logout_button).setOnClickListener(onClickListener);
+
+        }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) { //클릭된 위젯의 id를 이용
+            switch (v.getId()) {
+                case R.id.login_button2:
+                    //Login();
+                    break;
+                case R.id.resister_button:
+                    //startSignUpActivity();
+                    break;
+                case R.id.logout_button:
+                    //SignOut();
+            }
+        }
+    };
+    private void startToast(String msg){
+        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+    }
+    private void startSignUpActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+}
