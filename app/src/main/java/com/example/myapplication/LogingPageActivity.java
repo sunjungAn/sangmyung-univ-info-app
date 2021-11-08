a2fc16ef70cc42e46b7821e847fa884441a6556d
sunjungAn <dkstjswjd83@naver.com>
2021-11-08 14:42:12 +0900

    로그인 버튼 연결

--- app/src/main/java/com/example/myapplication/LogingPageActivity.java
+++ app/src/main/java/com/example/myapplication/LogingPageActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogingPageActivity extends AppCompatActivity {
 
+    Button loginbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loging_page);
+        loginbtn = findViewById(R.id.login_btn);
+        loginbtn.setOnClickListener(onClickListener);
+
+    }
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v){
+            switch(v.getId()){
+                case R.id.login_btn:
+                    Login();
+            }
+        }
+    };
+
+    private void Login(){
+        Intent intent = new Intent(this, tablemap.class);
+        startActivity(intent);
     }
 }
