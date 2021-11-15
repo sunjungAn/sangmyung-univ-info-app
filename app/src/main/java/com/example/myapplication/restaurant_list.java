92f18c83479fb5daa6ff9aabd3bb147360798f05
beeeeni <hyunbini2942@gmail.com>
2021-11-15 13:40:22 +0900

    ham's first commit

---
+++ app/src/main/java/com/example/myapplication/restaurant_list.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurant_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/restaurant_list2.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class restaurant_list2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list2);
+    }
+}
